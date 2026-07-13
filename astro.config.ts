d49409c471e93e513848297d1538991068b549e0
Terence Kim <jyterencekim@gmail.com>
2026-07-14 02:23:43 +0900

    upgrade astro 5 to 7, modernize deps, fix all vulnerabilities

--- astro.config.ts
+++ astro.config.ts
@@ -36,10 +36,6 @@ export default defineConfig({
     },
   },
   vite: {
-    // eslint-disable-next-line
-    // @ts-ignore
-    // This will be fixed in Astro 6 with Vite 7 support
-    // See: https://github.com/withastro/astro/issues/14030
     plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
@@ -57,8 +53,5 @@ export default defineConfig({
         optional: true,
       }),
     },
-  },
-  experimental: {
-    preserveScriptOrder: true,
   },
 });
